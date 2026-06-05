5ea8d09ae6d510e18cbe88781cef8d8117e45d7f
reyhanhmdani <rey7dan7@gmail.com>
2026-06-05 19:11:23 +0700

    refactor: penambahan fitur, menyimpan input di simpan di localstorage browser, tanpa kehilangan data ketika di refresh

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,19 +1,55 @@
-const form = document.getElementById("guestbookForm");
-const inputName = document.getElementById("inputName");
-const inputMessage = document.getElementById("inputMessage");
-const messageList = document.getElementById("messageList");
+const form = document.getElementById("expenseForm");
+const inputName = document.getElementById("expenseName");
+const inputNominal = document.getElementById("expenseAmount");
+const expensesList = document.getElementById("expensesList");
+const totalExpenses = document.getElementById("totalExpenses");
+
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+
+function renderPengeluaran() {
+  expensesList.innerHTML = "";
+  let total = 0;
+
+  for (let i = 0; i < expenses.length; i++) {
+    let item = expenses[i];
+
+    total = total + Number(item.amount);
+
+    const newListItem = document.createElement("li");
+    newListItem.className = "list-group-item px-0 py-3 d-flex justify-content-between align-items-center";
+
+    newListItem.innerHTML = `
+      <span class="text-dark fw-semibold small">${item.name}</span>
+      <span class="fw-bold text-danger small">Rp ${Number(item.amount).toLocaleString("id-ID")}</span>
+    `;
+
+    expensesList.appendChild(newListItem);
+  }
+
+  totalExpenses.innerText = `Rp ${Number(total).toLocaleString("id-ID")}`;
+}
 
 form.addEventListener("submit", function (event) {
   event.preventDefault(); // disini bikin data setelah di input itu ga ilang dulu sebelum di refresh
 
   const nameValue = inputName.value;
-  const messageValue = inputMessage.value;
+  const nominalValue = inputNominal.value;
 
-  const newListItem = document.createElement("li");
-  newListItem.innerHTML = `<strong>${nameValue}</strong>: "${messageValue}"`;
+  // validasi kalau input ga boleh kosong, dan memakai trim, supaya user ga bisa isen kalau "   " . . .q
+  if (nameValue.trim() === "" || Number(nominalValue) <= 0) {
+    alert("isi dulu bro");
+  } else {
+    const newExpense = {
+      name: nameValue,
+      amount: nominalValue,
+    };
 
-  // memasukkan element baru ke dalam list kita
-  messageList.appendChild(newListItem);
-
-  form.reset();
+    expenses.push(newExpense);
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+    renderPengeluaran();
+    form.reset();
+  }
 });
+
+// Panggil fungsi ini agar data dirender saat halaman pertama kali dibuka / direfresh
+renderPengeluaran();
